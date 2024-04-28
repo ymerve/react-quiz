a9fba01d60940a89e1219f3f1ed8409c1d64e6bc
merve <merve.yildirim@sparsetechnology.com>
2024-04-28 22:15:30 +0300

    Add finishing quiz.

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const FinishScreen = ({ points, maxPossiblePoints, highscore }) => {
+    const percentage = (points / maxPossiblePoints) * 100;
+
+    let emoji;
+    if (percentage === 100) emoji = '🏅';
+    if (percentage >= 80 && percentage < 100) emoji = '🥳';
+    if (percentage >= 50 && percentage < 80) emoji = '🙃';
+    if (percentage >= 0 && percentage < 50) emoji = '🤔';
+    if (percentage === 0) emoji = '🤦‍♂️';
+
+    return (
+        <>
+            <p className='result'>
+                <span>{emoji}</span> You scored <strong>{points} </strong>
+                out of {maxPossiblePoints}
+                ({Math.ceil(percentage)} %)
+            </p>
+            <p className='highscore'>(High Score : {highscore} points)</p>
+        </>
+    );
+}
+
+export default FinishScreen;
